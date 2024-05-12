ba36a8cfe8c6fd1d43c74ad5fbe3cb82afed2b47
SayuniEllepola <sayuni.elle@gmail.com>
2024-05-12 23:11:55 +0530

    To Do App Final

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("kotlin-parcelize")
-//    id("kotlin-kapt")
+    id("kotlin-kapt")
 }
 
 android {
@@ -60,25 +60,14 @@ dependencies {
 
     implementation("androidx.room:room-runtime:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
+    kapt("androidx.room:room-compiler:2.6.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
+
+    // Coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
 
 
-//    // ROOM
-//    val roomVersion = "2.6.1"
-//    implementation ("androidx.room:room-runtime:$roomVersion")
-//
-//    // Coroutines
-//    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
-//    implementation ("androidx.room:room-ktx:$roomVersion")
-//    // Navigation
-//    val navVersion = "2.7.5"
-//    implementation("androidx.navigation:navigation-fragment-ktx:$navVersion")
-//    implementation("androidx.navigation:navigation-ui-ktx:$navVersion")
-//    // Life Cycle Arch
-//    val lifecycleVersion = "2.6.2"
-//    // ViewModel
-//    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycleVersion")
-//    // LiveData
-//    implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycleVersion")
-//    // Annotation processor
+
+
 
 }
